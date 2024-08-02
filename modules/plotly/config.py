67b67a1b53a8198c02fc935c1f75bbb551560e4a
ibukikaburagi <63328418+1buk109@users.noreply.github.com>
2024-08-03 01:55:40 +0900

    chore: Add predefined color palette to Plotly configuration

--- modules/plotly/config.py
+++ modules/plotly/config.py
@@ -1,6 +1,18 @@
 import plotly.graph_objects as go
 import plotly.io as pio
 
+COLORS = {
+    "base": "#003366",
+    "primary": "#F56B60",
+    "secondary": "#20B2AA",
+    "success": "#28A745",
+    "info": "#17A2B8",
+    "warning": "#FFC107",
+    "danger": "#DC3545",
+    "light": "#F8F9FA",
+    "dark": "#343A40",
+}
+
 
 def set_custom_plotly_template(
     paper_bgcolor: str = "#fcfcfc",
